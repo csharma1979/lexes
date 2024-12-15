0e1658c998a91d2407235b723d6189e0c7fa68e1
Chandramohan Sharma <chandramohansharma@Chandramohans-MacBook-Air.local>
2024-12-15 14:45:06 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: '0.0.0.0',
+    port: 30002, // Replace 3001 with your desired port number
   },
 });
